fa2e4d22f36b29e01d4390651cb0e26dd70dda62
Five N <newfive1584@gmail.com>
2025-06-09 21:19:23 +0800

    add typecheck to precommit hooks and CI

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   // For frontend React/TypeScript files
-  'apps/frontend/**/*.{js,ts,tsx}': ['nx lint frontend'],
+  'apps/frontend/**/*.{js,ts,tsx}': ['nx lint frontend', 'nx typecheck frontend'],
 
   // For JavaScript/TypeScript files in the contracts library
-  'libs/contracts/**/*.{js,ts}': ['nx lint-js contracts'],
+  'libs/contracts/**/*.{js,ts}': ['nx lint-js contracts', 'nx typecheck contracts'],
 
   // For Solidity files in the contracts library
   'libs/contracts/**/*.sol': [
