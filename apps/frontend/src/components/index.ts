ad816811d75010dd2ac85cea449954ec7e528d18
Five N <newfive1584@gmail.com>
2025-06-26 12:05:04 +0800

    remove unused NetworkError component

--- apps/frontend/src/components/index.ts
+++ apps/frontend/src/components/index.ts
@@ -1,7 +1,6 @@
 export { WalletInfo } from './WalletInfo';
 export { Swap } from './Swap';
 export { Liquidity } from './Liquidity';
-export { NetworkError } from './NetworkError';
 export { ConnectWallet } from './ConnectWallet';
 export { ErrorBoundary } from './ErrorBoundary';
 export { ConnectedDashboard } from './ConnectedDashboard';
